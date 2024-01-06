ebb851a4e02e81529e8ab3f0589667109abe5852
Saurabh Yadav <yadav.saurabh9517@gmail.com>
2024-01-06 22:10:28 +0530

    Final

--- app/src/main/java/com/sytechnosoft/agecalculator/MainActivity.java
+++ app/src/main/java/com/sytechnosoft/agecalculator/MainActivity.java
@@ -348,20 +348,18 @@ public class MainActivity extends AppCompatActivity {
                         // The mInterstitialAd reference will be null until
                         // an ad is loaded.
                         mInterstitialAd = interstitialAd;
-                        Log.i(TAG, "onAdLoaded");
 
                         mInterstitialAd.setFullScreenContentCallback(new FullScreenContentCallback(){
                             @Override
                             public void onAdClicked() {
                                 // Called when a click is recorded for an ad.
-                                Log.d(TAG, "Ad was clicked.");
                             }
 
                             @Override
                             public void onAdDismissedFullScreenContent() {
                                 // Called when ad is dismissed.
                                 // Set the ad reference to null so you don't show the ad a second time.
-                                Log.d(TAG, "Ad dismissed fullscreen content.");
+                               // Log.d(TAG, "Ad dismissed fullscreen content.");
                                 mInterstitialAd = null;
                                 onBackPressed();
                             }
@@ -369,20 +367,20 @@ public class MainActivity extends AppCompatActivity {
                             @Override
                             public void onAdFailedToShowFullScreenContent(AdError adError) {
                                 // Called when ad fails to show.
-                                Log.e(TAG, "Ad failed to show fullscreen content.");
+                               // Log.e(TAG, "Ad failed to show fullscreen content.");
                                 mInterstitialAd = null;
                             }
 
                             @Override
                             public void onAdImpression() {
                                 // Called when an impression is recorded for an ad.
-                                Log.d(TAG, "Ad recorded an impression.");
+                               // Log.d(TAG, "Ad recorded an impression.");
                             }
 
                             @Override
                             public void onAdShowedFullScreenContent() {
                                 // Called when ad is shown.
-                                Log.d(TAG, "Ad showed fullscreen content.");
+                                //Log.d(TAG, "Ad showed fullscreen content.");
                             }
                         });
                     }
@@ -390,7 +388,7 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onAdFailedToLoad(@NonNull LoadAdError loadAdError) {
                         // Handle the error
-                        Log.d(TAG, loadAdError.toString());
+                      //  Log.d(TAG, loadAdError.toString());
                         mInterstitialAd = null;
                     }
                 });
